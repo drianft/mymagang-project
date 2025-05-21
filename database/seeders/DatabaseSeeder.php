5f3b89be66d701ccc11a7a2caa66397a37058600
David-Wijaya-241402040 <180739063+David-Wijaya-241402040@users.noreply.github.com>
2025-05-21 14:11:30 +0700

    Jetstream Baru

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,25 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $faker = Faker::create('id_ID');
-        for($i = 1; $i <= 20; $i++){
-            DB::table('users')->insert([
-                'email' => $faker->email,
-                'email_verified_at' => now(),
-                'password' => bcrypt('password'),
-                'status' => 'active',
-                'role' => 'applier',
-                'remember_token' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
-                'fullName' => $faker->name,
-                'address' => $faker->address,
-                'birth_date' => $faker->date,
-            ]);
-        }
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }

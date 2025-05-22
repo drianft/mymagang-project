430fd3f628f490c54dac173829df079e2c6eabb0
David-Wijaya-241402040 <180739063+David-Wijaya-241402040@users.noreply.github.com>
2025-05-22 08:35:36 +0700

    Guest Features Done (FIX)

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -75,6 +75,22 @@
                         </div>
                     </div>
                 @endauth
+                @guest  
+                <div class="relative ml-3">
+                    <div>
+                    <button type="button" class="relative flex rounded-full bg-zinc-700 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-zinc-700 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <span class="absolute -inset-1.5"></span>
+                        <span class="sr-only">Open user menu</span>
+                        <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    </button>
+                    </div>
+
+                    <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                    <a href="{{ route('login') }}" class="block px-4 py-2 text-sm text-gray-700 hover:text-black hover:font-semibold transition duration-150 px-4 py-2 ease-in-out text-sm" role="menuitem" tabindex="-1" id="user-menu-item-0">Login</a>
+                    <a href="{{ route('register') }}" class="block px-4 py-2 text-sm text-gray-700 hover:text-black hover:font-semibold transition duration-150 px-4 py-2 ease-in-out text-sm" role="menuitem" tabindex="-1" id="user-menu-item-1">Sign In</a>
+                    </div>
+                </div>
+                @endguest
             </div>
         </div>
     </div>

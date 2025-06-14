5655f28e0f8551514f497dcb078a08bd2390ac8b
drianft <drianft86@gmail.com>
2025-06-14 20:59:27 +0700

    Web 60%

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 
         <div class=" {{ (!$user) ? 'min-h-screen bg-white' : ($user->roles == 'admin' ? 'flex min-h-screen bg-gray-100' : 'min-h-screen bg-white') }}">
 
-            @if(!$user || $user && $user->roles != 'applier')
+            @if(!$user || $user && $user->roles == 'applier')
                 @include('components.navbar')
             @elseif($user && $user->roles == 'admin')
                 @include('components.admin-sidebar')
